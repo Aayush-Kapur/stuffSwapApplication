3f5ac92cb88f8bb801339587d5c3fa8b2053f932
Aayush-Kapur <aayu.kapur@gmail.com>
2024-06-18 21:52:57 +0530

    Added log statement

--- src/main/java/com/aayush/studentStuffSwap/studentStuffSwap/controller/UserController.java
+++ src/main/java/com/aayush/studentStuffSwap/studentStuffSwap/controller/UserController.java
@@ -3,6 +3,7 @@ package com.aayush.studentStuffSwap.studentStuffSwap.controller;
 import com.aayush.studentStuffSwap.studentStuffSwap.InputDataTransferObject.UserUpdateDTO;
 import com.aayush.studentStuffSwap.studentStuffSwap.OutputDataTransferObject.UserOutputDTO;
 import com.aayush.studentStuffSwap.studentStuffSwap.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
     @Autowired
     private UserService userService;
@@ -27,6 +29,7 @@ public class UserController {
 
     @PutMapping("/update")
     public ResponseEntity<UserOutputDTO> updateUser(@RequestBody UserUpdateDTO userUpdateDTO) {
+        log.info("Call received to updateUser");
        return userService.updateUser(userUpdateDTO);
 
     }
